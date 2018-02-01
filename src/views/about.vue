1ba32702edeef9f0b53fa663fa3a76399e9c39af
Nick Chang <changjie22@qq.com>
2018-02-01 15:54:41 +0800

    Update about.vue

--- src/views/about.vue
+++ src/views/about.vue
@@ -5,15 +5,6 @@
              :fix-head="true"
              :need-add="true">  <!--动态 Props,每当父组件的数据变化时，就会传导给子组件-->
     </nv-head> <!--父组件向子组件传递prop数据-->
-    <dl class="about-info">
-      <dt>关于项目</dt>
-      <dd>该项目是基于cNode的API，采用vue2.0重些的webapp</dd>
-      <dt>源码地址</dt>
-      <dd>
-        <a href="https://github.com/shinygang/Vue-cnodejs">
-        https://github.com/shinygang/Vue-cnodejs</a>
-      </dd>
-    </dl>
   </div>
 </template>
 
